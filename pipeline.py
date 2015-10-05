d6ed52d30ab17435570f3a8a60655149882c3fe2
Arkiver2 <Arkiver@hotmail.com>
2015-10-06 00:58:24 +0200

    accept exit code 4. bump version.

--- pipeline.py
+++ pipeline.py
@@ -57,7 +57,7 @@ if not WGET_LUA:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = "20151005.03"
+VERSION = "20151005.04"
 USER_AGENT = 'ArchiveTeam'
 TRACKER_ID = 'rutracker'
 TRACKER_HOST = 'tracker.archiveteam.org'
@@ -237,7 +237,7 @@ pipeline = Pipeline(
     WgetDownload(
         WgetArgs(),
         max_tries=2,
-        accept_on_exit_code=[0, 8],
+        accept_on_exit_code=[0, 4, 8],
         env={
             "item_dir": ItemValue("item_dir"),
             "item_value": ItemValue("item_value"),
