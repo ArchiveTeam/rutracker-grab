17bdd6bb72424586b76f9c1d924c186bd50cd15e
Arkiver2 <Arkiver@hotmail.com>
2015-10-06 22:26:51 +0200

    ignore problematic images. bump version.

--- pipeline.py
+++ pipeline.py
@@ -57,7 +57,7 @@ if not WGET_LUA:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = "20151005.04"
+VERSION = "20151006.01"
 USER_AGENT = 'ArchiveTeam'
 TRACKER_ID = 'rutracker'
 TRACKER_HOST = 'tracker.archiveteam.org'
